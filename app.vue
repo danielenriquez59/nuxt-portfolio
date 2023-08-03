0673b1d5db8fc73f52b73fc94df6a1b2f44980c8
danielenriquez59 <deenriquez92@gmail.com>
2023-08-03 10:17:17 -0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="relative overflow-hidden">
+    <the-nav></the-nav>
+    <NuxtPage />
+    <the-footer></the-footer>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  devtools: { enabled: true },
+  css:[
+    '~/assets/css/main.css',
+    "primevue/resources/themes/lara-light-blue/theme.css",
+    "primevue/resources/primevue.css",
+    'primeicons/primeicons.css'
+  ],
+  build: {
+		transpile: ["primevue"],
+	},
+})
